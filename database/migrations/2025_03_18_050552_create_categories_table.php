1bea23f80fca6b91150314508c3f34264d41ec1e
Noman Mia <nomanislamnishad@gmail.com>
2025-03-18 11:36:23 +0600

    added migration all

--- database/migrations/2025_03_18_050552_create_categories_table.php
+++ database/migrations/2025_03_18_050552_create_categories_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('user-id');
+            $table->foreign('user-id')->references('id') ->on('users')
+            ->cascadeOnUpdate()->cascadeOnDelete();
             $table->timestamps();
         });
     }
