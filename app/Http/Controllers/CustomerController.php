55db615fb534a5a4ccd9584266aba37d2b22648c
Noman Mia <nomanislamnishad@gmail.com>
2025-03-26 15:24:49 +0600

    added invoice

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -8,50 +8,62 @@ use Illuminate\Http\Request;
 class CustomerController extends Controller
 {
    
-    public function createCategory(Request $request){
+    public function createCustomer(Request $request){
 
         $user_id = $request->header('id');
 
+        $request->validate([
+            'name' => 'required',
+             'email'=>'required|email|unique:customers,email',
+             'mobile'=>'required',
+        ]);
         Customer::create([
-            'name' => $request->name,
+            'name'=>$request->input('name'),
+            'email'=>$request->input('email'),
+            'mobile'=>$request->input('mobile'),            
             'user_id' => $user_id,
         ]); 
         return response()->json([
             'status'=>'success',
-            'message'=>'Category created successfully'
+            'message'=>'Customer created successfully'
         ],200);
 
     } //end method
 
-    public function categoryList(Request $request){
+    public function CustomerList(Request $request){
         $user_id = $request->header('id');
-        $category = Customer::where('user_id',$user_id)->get();
-        return $category;
+        $customers = Customer::where('user_id',$user_id)->get();
+        return $$customers;
     } //end method
 
-    public function categoryById(Request $request){
+    public function CustomerById(Request $request){
         $category = Customer::find($request->id);
         return $category;
     } //end method
 
 
-    public function CategoryUpdate(Request $request){
-        $category = Customer::find($request->id);
-        $category->name = $request->name;
-        $category->save();
+    public function CustomerUpdate(Request $request){
+        $user_id = $request->header('id');
+        $id = $request->input('id');
+        Customer::where('id',$id)->update([
+            'name'=>$request->input('name'),
+            'email'=>$request->input('email'),
+            'mobile'=>$request->input('mobile'),
+            'user_id' => $user_id,
+        ]);
         return response()->json([
             'status'=>'success',
-            'message'=>'Category updated successfully'
+            'message'=>'Customer updated successfully'
         ],200);
     }//end method
 
 
-    public function CategoryDelete(Request $request){
-        $category = Customer::find($request->id);
-        $category->delete();
+    public function CustomerDelete(Request $request){
+        $customer = Customer::find($request->id);
+        $customer->delete();
         return response()->json([
             'status'=>'success',
-            'message'=>'Category deleted successfully'
+            'message'=>'Customer deleted successfully'
         ],200);
     }//end method
 
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\InvoiceController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 use App\Http\Middleware\TokenVerificationMiddleware;
@@ -66,8 +67,10 @@ Route::middleware(TokenVerificationMiddleware::class)->group(function(){
     ->name('CustomerById');
     Route::post('/update-customer',[CustomerController::class,'CustomerUpdate'])
     ->name('CustomerUpdate');
-    Route::get('/delete-customer/{id}', [CustomerController::class,'CustomerDelete']);
+    Route::get('/delete-customer/{id}', [CustomerController::class,'CustomerDelete'])->name('CustomerDelete');
 
+    //invoice all routes
+    Route::post('/invoice-create',[InvoiceController::class,'InvoiceCreate'])->name('InvoiceCreate');
 
 });
 
